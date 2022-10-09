5581acff73ba3daa734d65325d3646ed8d509411
Daniel Gomez <dgomez.informatica@gmail.com>
2022-10-09 02:33:51 -0300

    Clase principal donde se llama al formulario Agenda_Telefonica

---
+++ src/com/agenda/Agenda_Telefonica.java
@@ -0,0 +1,13 @@
+package com.agenda;
+
+import com.agenda.vista.AgendaTelefonicaFrame;
+
+public class Agenda_Telefonica {
+
+	public static void main(String[] args) {
+		AgendaTelefonicaFrame agendaT = new AgendaTelefonicaFrame();
+		agendaT.setVisible(true);
+
+	}
+
+}
