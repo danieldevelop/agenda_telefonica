00f154bc8d62f57c90857aae2e47780464f8ff81
Daniel Gomez <dgomez.informatica@gmail.com>
2022-10-12 03:20:55 -0300

    Se crea clase generica para tipos de alertas JOptionPane

---
+++ src/com/agenda/functions/MensajeBox.java
@@ -0,0 +1,29 @@
+/**
+ * En este paquete se crea la funciones generales que el proyecto requiera
+ * para los formularios del proyecto, entre otros. 
+ */
+package com.agenda.functions;
+
+import javax.swing.JOptionPane;
+import javax.swing.JRootPane;
+
+/**
+ * Clase para mostrar mensaje personalizado genericos
+ * 
+ * Valores predeterminados iconos JOptionPane
+ * 	JOPtionPane.PLAIN_MESSAGE = -1
+ * 	JOPtionPane.ERROR_MESSAGE = 0
+ * 	JOPtionPane.INFORMATION_MESSAGE = 1
+ * 	JOPtionPane.WARNING_MESSAGE = 2
+ * 	JOPtionPane.QUESTION_MESSAGE = 3
+ * 
+ * @author Daniel Gómez
+ */
+public class MensajeBox {
+	
+	private static final String TITULO = "Agenda Telefonica"; 	
+	
+	public void showMessageDialog(JRootPane contentPane, String mensaje, Integer icono) {
+		JOptionPane.showMessageDialog(contentPane, mensaje, TITULO, icono);
+	}
+}
